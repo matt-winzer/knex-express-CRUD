2b62e40b2dd39ebf4b3e0e4998318daf014239e5
matt-winzer <matt.winzer@gmail.com>
2016-12-08 16:59:48 -0700

    updated .env

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://localhost/cheese'
+    connection: process.env.LOCAL_DATABASE_URL
   },
 
   production: {
